fb3d30159f488ef5d51f231abc40e0634d94cf2e
moh. nurwahid <mohnurwaid923@gmail.com>
2026-03-10 12:53:46 +0700

    membuat display

--- PERTEMUAN4/Insertion_sort.cpp
+++ PERTEMUAN4/Insertion_sort.cpp
@@ -47,3 +47,15 @@ void insertionsort(){
         }
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array" << endl;
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << arr endl;
+    }
+    cout << endl;
+}
