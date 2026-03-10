0dc43570173e79c59e50197730eaa763b92fbfdd
moh. nurwahid <mohnurwaid923@gmail.com>
2026-03-10 12:27:22 +0700

    membuat prosedur input

--- PERTEMUAN4/Insertion_sort.cpp
+++ PERTEMUAN4/Insertion_sort.cpp
@@ -3,3 +3,25 @@ using namspace std;
 
 int arr[20];
 int n;
+
+void input(){
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <=20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "==============================================";
+    cout << "============Masukkan Element Array============";
+    cout << "==============================================";
+
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i+i) << ": ";
+    }
+}
